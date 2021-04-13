21fcb13544e2d615b35a4d1924d7d5011f1fdd94
Jehyeop_Yu <wpguq96@naver.com>
2021-04-13 20:07:55 +0900

    Game-Action-page

--- JavaScript/GameDi.js
+++ JavaScript/GameDi.js
@@ -5,38 +5,20 @@ var startClick = function(){
 }
 
 var edit = function(){
-    document.getElementsByTagName('input')[0].style.display="block";
-    document.getElementsByTagName('input')[1].style.display="block";
-    document.getElementsByTagName('input')[2].style.display="block";
-    document.getElementsByTagName('input')[3].style.display="block";
-    document.getElementsByTagName('input')[4].style.display="block";
-    document.getElementsByTagName('input')[5].style.display="block";
+    var inputList = document.querySelectorAll('input')
+    for(var i=0; i<inputList.length; i++){
+        document.getElementsByTagName('input')[i].style.display="block";
+    }
 }
 
 var save = function(){
-    document.getElementsByTagName('input')[0].style.display="none";
-    document.getElementsByTagName('input')[1].style.display="none";
-    document.getElementsByTagName('input')[2].style.display="none";
-    document.getElementsByTagName('input')[3].style.display="none";
-    document.getElementsByTagName('input')[4].style.display="none";
-    document.getElementsByTagName('input')[5].style.display="none";
+    var inputList = document.querySelectorAll('input')
+    for(var i=0; i<inputList.length; i++){
+        document.getElementsByTagName('input')[i].style.display="none";
+    }
 }
 
-
-
-
-
-
-
-
-
-
-// 수정01
-// var add = function() {
-//     var name = prompt('Menu Name','');
-//     var plusUl = document.createElement('li');
-//     plusUl.innerHTML = "<a href="+name+".html"+">"+name+"</a>";
-//     if(!name == '') {
-//         document.getElementById('MENU-add').appendChild(plusUl);
-//     }
-// }
+var Action = function(){ 
+    document.querySelector('div.Game-main').style.display="none";
+    document.querySelector('div.Game-Action').style.display="block";
+}
